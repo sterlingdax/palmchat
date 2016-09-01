882db6b0bb8485a3d68445435e7e0571123c4d22
Sterling <sterlingdax@users.noreply.github.com>
2016-09-01 11:47:08 -0400

    topic changes
    Still problems with string formatting, might just be better to move the
    entire thing to Python 3

--- palmchat.py
+++ palmchat.py
@@ -26,53 +26,62 @@ time.sleep(2)
 print ">_%s: Hello " % random.choice (users) + "%s" % login
 print ">_%s: Hi" % random.choice (users)
 
+
+# These have to be moved below!! Can't run without first defining them
 # Topic class, includes start 'room' as well as any programmed topics
-class topic(object):
-	def conversation(self):
-		prompt = raw_input(">_%s") % login
-		if "ham" in prompt:
-			return 'ham'
-		if "bush" in prompt:
-			return 'bush'
-		if "obama" in prompt:
-			return 'obama'
+def topic():
+	prompt = raw_input(">_%s") % login
+	if "ham" in prompt:
+		return 'ham'
+	if "bush" in prompt:
+		return 'bush'
+	if "obama" in prompt:
+		obama()
 
-class bush(topic):
-	def conversation(self):
-		print ">_aaron: I blame Bush!"
-		print ">_aaron: come to think of it..OBAMA too!"
-		print ">_pilot518: None of them top Reagan, amirite?", login
 
-		reply = raw_input(">_%s:") % login
+def bush():
+	print ">_aaron: I blame Bush!"
+	print ">_aaron: come to think of it..OBAMA too!"
+	print ">_pilot518: None of them top Reagan, amirite?", login
+
+	reply = raw_input(">_%s:") % login
 		
-		if reply == "yes":
-			print ">_pilot518: Mr. Gorbachev, tear down this wall!"
-			return 'topic'
-		elif reply == "no":
-			print ">_aaron: I blame Bush!"
-			return 'topic'
-		else:
-			print ">_aaron: What are you, some sort of Tea party Hack?"
-			return 'topic'
+	if reply == "yes":
+		print ">_pilot518: Mr. Gorbachev, tear down this wall!"
+		return 'topic'
+	elif reply == "no":
+		print ">_aaron: I blame Bush!"
+		return 'topic'
+	else:
+		print ">_aaron: What are you, some sort of Tea party Hack?"
+		return 'topic'
 
-class ham(topic):
-	def conversation(self):
-		print ">_Mike_W: I like radios!"
-		print ">_irq: You mean HAM radio, if you going to talk about it, get it right!"
-		print ">_Mike_W: ...What do you think", login, "does it matter?"
 
-		reply = raw_input(">_%s:") % login
+def ham():
+	print ">_Mike_W: I like radios!"
+	print ">_irq: You mean HAM radio, if you going to talk about it, get it right!"
+	print ">_Mike_W: ...What do you think", login, "does it matter?"
 
-		if reply == "yes":
-			print ">_irq: Of course it matters,", login, "agrees"
-			return 'topic'
-		elif reply == "no":
-			print ">_Mike_W: See,", login, "gets the point, it's time you did!"
-			return 'topic'
-		else:
-			print ">_irq: If you didn't care, why did you ask?"
-			return 'topic'
+	reply = raw_input(">_%s:") % login
 
-class obama(topic):
-	def conversation(self):
+	if reply == "yes":
+		print ">_irq: Of course it matters,", login, "agrees"
+		return 'topic'
+	elif reply == "no":
+		print ">_Mike_W: See,", login, "gets the point, it's time you did!"
+		return 'topic'
+	else:
+		print ">_irq: If you didn't care, why did you ask?"
+		return 'topic'
+
+
+def obama():
+	print ">_aaron: oh Obama, don't you lie to me..."
+	print ">_pthree: Shut your mouth! I could afford to have two kids under obamacare!"
+	print ">_Worf: Canada's medicine has a waiting list"
+	return 'topic'
+
+
+
+topic()
 
