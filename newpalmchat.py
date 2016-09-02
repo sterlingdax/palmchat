946cc8c5f3e2b96774f7d37d4bdbbea2a1f64e40
Sterling <sterlingdax@me.com>
2016-09-01 23:35:13 -0400

    Starting over, cry a little

---
+++ newpalmchat.py
@@ -0,0 +1,32 @@
+# New version of Palmchat game
+
+# topic ideas: obama, trump, iphone, ipad, sysadmin
+
+# usernames: aaron, Austin, Cloud_Strife, CosmicRay, dampeoples, irq, liiwi, LonEagle, Mike_W, pilot518, pthree, tomcatt1, Worf
+
+# channel topic - Eat your drugs, stay in vegetables and don't do school.
+
+# Syntax for the login:
+# print('Comput-0:~ ' + UserName + '$'))
+
+def trump():
+	print('<irq>what the literal fuck?')
+	print('<irq>if that situation happened to me, i would hunt down the guy who put a password in it and destroy his family and his next 3 generations of family')
+	return 'topic'
+
+
+def prompt():
+	topic = input('[#palmchat] ')
+	if 'trump' in topic:
+		return 'trump'
+		
+
+
+
+
+
+
+
+prompt()
+
+	
